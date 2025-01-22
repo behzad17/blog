c7ad2e2fff183ed4f22f0775aa3c79c9ae5d2c15
BJ <158008244+behzad17@users.noreply.github.com>
2025-01-22 21:17:20 +0000

    correct host adress

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7)^j8h0vac+end3dgnqw060i^&oab%p3^_oql)!&tn^gxf+er*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000','localhost', '127.0.0.1''.herokuapp.com']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000','localhost', '127.0.0.1','.herokuapp.com']
 
 
 
