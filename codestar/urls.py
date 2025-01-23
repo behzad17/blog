4bdbbaa9df1f173aa5b0e339290fa574be4913b8
BJ <158008244+behzad17@users.noreply.github.com>
2025-01-23 00:31:32 +0000

    correct some fel in url file

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
+from blog import views as blog_views
 
 
 #urlpatterns = [
