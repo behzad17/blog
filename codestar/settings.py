5c3199a07bdb624ced764722f68ff44ae7cd899b
Behzad Javadian <behzad_1349@hotmail.com>
2025-01-29 18:59:59 +0000

    uppdate allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,9 +30,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000','localhost','blog-1717.herokuapp.com']
+#ALLOWED_HOSTS = ['127.0.0.1:8000','localhost','blog-1717.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'blog-1717.herokuapp.com', '8000-behzad17-blog-a6evvtgipmo.ws.codeinstitute-ide.net']
+#ALLOWED_HOSTS = ['*']
+
 
 
 
