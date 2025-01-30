4fb65d49ac5815d5f0990db5e7386f5200f045cd
Behzad Javadian <behzad_1349@hotmail.com>
2025-01-30 00:19:14 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '8000-behzad17-blog-a6evvtgipmo.ws.codeinstitute-ide.net']
 
